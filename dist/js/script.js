164c648584613d385a33ac984c8a3237ae1d4907
ValLR <valentinalopezrecabal@gmail.com>
2017-08-29 15:47:15 -0300

    cambios mapa inicial

--- dist/js/script.js
+++ dist/js/script.js
@@ -12630,4 +12630,12 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-alert("hola mundo");
+function initMap(){
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 8, 
+		center:{lat:-33.4430, lng: -70.6619},
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl:false
+	});
+}
