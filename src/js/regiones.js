ecaa9a6abf45bcb9893a2dfcaf415a60847a5c38
vanegarciay <vanegarciay@gmail.com>
2017-08-30 15:14:15 -0300

    Update regiones.js

--- src/js/regiones.js
+++ src/js/regiones.js
@@ -139,4 +139,118 @@ var regiones = [
 		"latitud":-53.13351979999999,
 		"longitud":-70.9110349
 	}
+	{
+    "id": 70,
+    "comuna": "Viña del Mar",
+    "latitud": -33.0153481,
+    "longitud": -71.55002760000002
+},
+{
+    "id": 71,
+    "comuna": "Valparaíso",
+    "latitud": -33.047238,
+    "longitud": -71.61268849999999
+},
+{
+    "id": 72,
+    "comuna": "La Calera",
+    "latitud": -32.7877958,
+    "longitud": -71.20399629999997
+},
+{
+    "id": 73,
+    "comuna": "San Felipe",
+    "latitud": -32.75,
+    "longitud": -70.72081149999997
+},
+{
+    "id": 74,
+    "comuna": "Quilpué",
+    "latitud": -33.0482707,
+    "longitud": -71.4408752
+},
+{
+    "id": 75,
+    "comuna": "Quilicura",
+    "latitud": -33.0573408,
+    "longitud": -71.60442319999999
+},
+{
+    "id": 76,
+    "comuna": "La Reina",
+    "latitud": -33.4411269,
+    "longitud": -70.53405909999998
+},
+{
+    "id": 77,
+    "comuna": "Las Condes",
+    "latitud": -33.3989812,
+    "longitud": -70.5573124
+},
+{
+    "id": 78,
+    "comuna": "Maipu",
+    "latitud": -33.5209155,
+    "longitud": -70.76313349999998
+},
+{
+    "id": 79,
+    "comuna": "La Florida",
+    "latitud": -33.5226882,
+    "longitud": -70.59871420000002
+},
+{
+    "id": 80,
+    "comuna": "Huechuraba",
+    "latitud": -33.3742128,
+    "longitud": -70.63674249999997
+},
+{
+    "id": 81,
+    "comuna": "Puente Alto",
+    "latitud": -33.6186082,
+    "longitud": -70.59060569999997
+},
+{
+    "id": 82,
+    "comuna": "San Miguel",
+    "latitud": -33.4923545,
+    "longitud": -33.4923545
+},
+{
+    "id": 83,
+    "comuna": "Los Cerrillos",
+    "latitud": -33.5041325,
+    "longitud": -70.71089949999998
+},
+{
+    "id": 84,
+    "comuna": "Lo Barnechea",
+    "latitud": -33.352669,
+    "longitud": -70.51851699999997
+},
+{
+    "id": 85,
+    "comuna": "Providencia",
+    "latitud":-33.4314474,
+    "longitud": -70.6093325
+},
+{
+    "id": 86,
+    "comuna": "Santiago",
+    "latitud": -33.4566678,
+    "longitud": -70.59784150000002
+},
+{
+    "id": 87,
+    "comuna": "San Bernardo",
+    "latitud": -33.5854485,
+    "longitud": -70.69873610000002
+},
+{
+    "id": 88,
+    "comuna": "Cerrillos",
+    "latitud": -33.5041325,
+    "longitud": -70.1356692
+},
 ]
