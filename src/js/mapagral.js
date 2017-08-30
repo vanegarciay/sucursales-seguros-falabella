a9d8a85622294f96d9776e168da63fff1706fc57
VaneGarciaYara <vanegarciay@gmail.com>
2017-08-29 22:42:32 -0300

    integrando las sucursales al mapa

--- src/js/mapagral.js
+++ src/js/mapagral.js
@@ -1,5 +1,6 @@
 function initMap(){
     $(document).ready(function() {
+        
     	var map = new google.maps.Map(document.getElementById("map"), {
     		zoom: 8,
     		center:{lat:-33.4430, lng: -70.6619},
@@ -8,9 +9,44 @@ function initMap(){
     		streetViewControl:false
     	});
 
-        sucursales.forEach(function(element){
-            console.log(element.nombre);
-        });
+        marcarSucursalesEnMapa(sucursales);
+
+        function marcarSucursalesEnMapa(sucursales) {
+
+            sucursales.forEach(function(element){
+                var lat = element.latitud;
+                var lon = element.longitud;
+            console.log(lat);
+            console.log(lon);
+            marcarSucursal(lat, lon);
+            });
+        }
+
+        function marcarSucursal(lat, lon) {
+            var marker = crearMarcador(map);
+
+            marker.setPosition(new google.maps.LatLng(lat,lon));
+            marker.setVisible(true);
+        }
+
+        function crearMarcador(map) {
+            var icono = {
+                url: 'http://www.myiconfinder.com/uploads/iconsets/82a679a558f2fe4c3964c4123343f844.png',
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(35, 35)
+            };
+
+            var marker = new google.maps.Marker({
+                map: map,
+                animation: google.maps.Animation.DROP,
+                icon: icono,
+                anchorPoint: new google.maps.Point(0, -29)
+            });
+
+            return marker;
+        }
 
 });
 
