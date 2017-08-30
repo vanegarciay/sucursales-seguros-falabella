55813a60e041dc40fceee53d074ac0bce9e20e10
ValLR <valentinalopezrecabal@gmail.com>
2017-08-30 14:40:48 -0300

    json regiones

---
+++ src/js/regiones.js
@@ -0,0 +1,80 @@
+var regiones = [
+	{
+		"id": 57,
+		"region":"Región de Tarapacá",
+		"latitud": -20.2328758,
+		"longitud": -70.14290879999999		
+	},
+	{
+		"id": 58,
+		"region":"Región de Antofagasta",
+		"latitud": -23.5008275,
+		"longitud": -68.8636491,		
+	},
+	{
+		"id": 59,
+		"region":"Región de Atacama",
+		"latitud": -27.3693267,
+		"longitud": -70.33958419999999		
+	},
+	{
+		"id": 60,
+		"region":"Región de Coquimbo",
+		"latitud": -30.6378386,
+		"longitud": -73.0080073		
+	},
+	{
+		"id":61,
+		"region":"Región de Valparaíso",
+		"latitud":-2.1745881,
+		"longitud":-175.6881946
+	},
+	{
+		"id":62,
+		"region":"Región Metropolitana",
+		"latitud":-33.5787913,
+		"longitud":-72.9829728		
+	},
+	{
+		"id":63,
+		"region":"Región de O'Higgins",
+		"latitud":-34.4051864,
+		"longitud":-73.2847002
+	},
+	{
+		"id":64,
+		"region":"Región del Maule",
+		"latitud":-35.536349,
+		"longitud":-76.049239
+	},
+	{
+		"id":65,
+		"region":"Región del Biobío",
+		"latitud":-37.1563717,
+		"longitud":-76.9682359
+	},
+	{
+		"id":66,
+		"region":"Región de los Ríos",
+		"latitud":-39.9586742,
+		"longitud":-74.8977886
+	},
+	{
+		"id":67,
+		"region":"Región de la Araucanía",
+		"latitud":-38.7446848,
+		"longitud":-72.58974660000001
+	}
+	{
+		"id":68,
+		"region":"Región de los Lagos",
+		"latitud":-42.0558395,
+		"longitud":-77.706636
+	}
+	{
+		"id":69,
+		"region":"Región de Magallanes",
+		"latitud":-53.13351979999999,
+		"longitud":-70.9110349
+	}
+]
