9f7c5b0e34b88922bdb734e4ecbe724a389d277b
ValLR <valentinalopezrecabal@gmail.com>
2017-08-29 21:30:44 -0300

    proto impresión

--- dist/js/script.js
+++ dist/js/script.js
@@ -12642,6 +12642,13 @@ function initMap(){
 
         sucursales.forEach(function(element){
             console.log(element.nombre);
+            $("#sucursales-printed").append(
+            `<div class="col-md-5 col-lg-5 col-xs-12">
+                  <h4>`+ element.nombre +`</h4>
+                  <p>`+ element.title +`</p>
+                  <p>`+ element.direccion +`</p>
+                  <p>`+ element.comuna +`, `+ element.region +`</p>
+            </div>`);
         });
 
 });
