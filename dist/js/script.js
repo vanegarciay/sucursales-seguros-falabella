ec0ddb38dea593b02821d74bffbe534615c3cd1d
ValLR <valentinalopezrecabal@gmail.com>
2017-08-30 03:02:00 -0300

    filtros, función

--- dist/js/script.js
+++ dist/js/script.js
@@ -12632,12 +12632,44 @@ if (typeof jQuery === 'undefined') {
 
 $(document).ready(function(){
 	$("#option_region").change(function(){
-		if(($("#option_region").val())=="Región de Tarapacá"){
-			if((($(".cuadro-sucursal")).is("#1"))=== false){
-				$(".cuadro-sucursal").addClass("hide");
-			}
-
+		$("#sucursales-printed").empty();
+		var region =$("#option_region").val();
+		if(($("#option_region").val())=== "Región de Tarapacá"){
+			console.log(sucursales[0].nombre)
+			$("#sucursales-printed").append(`
+			<div class="cuadro-sucursal col-md-5 col-lg-5 col-xs-12 id=`+ sucursales[0].id +`">
+                  <h4>`+ sucursales[0].nombre +`</h4>
+                  <p>`+ sucursales[0].title +`</p>
+                  <p>`+ sucursales[0].direccion +`</p>
+                  <p>`+ sucursales[0].comuna +`, `+ sucursales[0].region +`</p>
+                  <p><b>Horario:</b> `+ sucursales[0].horario +`</p>
+            </div>
+				`)
 		}
+		else if(($("#option_region").val())=== "Región de Atacama"){
+			console.log(sucursales[3].nombre)
+			$("#sucursales-printed").append(`
+			<div class="cuadro-sucursal col-md-5 col-lg-5 col-xs-12 id=`+ sucursales[3].id +`">
+                  <h4>`+ sucursales[3].nombre +`</h4>
+                  <p>`+ sucursales[3].title +`</p>
+                  <p>`+ sucursales[3].direccion +`</p>
+                  <p>`+ sucursales[3].comuna +`, `+ sucursales[3].region +`</p>
+                  <p><b>Horario:</b> `+ sucursales[3].horario +`</p>
+            </div>
+				`)
+		}
+		else if(($("#option_region").val())=== "Región de Magallanes"){
+			console.log(sucursales[55].nombre)
+			$("#sucursales-printed").append(`
+			<div class="cuadro-sucursal col-md-5 col-lg-5 col-xs-12 id=`+ sucursales[55].id +`">
+                  <h4>`+ sucursales[55].nombre +`</h4>
+                  <p>`+ sucursales[55].title +`</p>
+                  <p>`+ sucursales[55].direccion +`</p>
+                  <p>`+ sucursales[55].comuna +`, `+ sucursales[55].region +`</p>
+                  <p><b>Horario:</b> `+ sucursales[55].horario +`</p>
+            </div>
+				`)
+		}	
 	});
 });
 function initMap(){
@@ -12674,9 +12706,10 @@ function initMap(){
                   <p>`+ element.comuna +`, `+ element.region +`</p>
                   <p><b>Horario:</b> `+ element.horario +`</p>
             </div>`);
+            
             marcarSucursal(lat, lon);
             });
-            
+
         }
 
         function marcarSucursal(lat, lon) {
---
+++ src/js/filter.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$("#option_region").change(function(){
+		$("#sucursales-printed").empty();
+		var region =$("#option_region").val();
+		if(($("#option_region").val())=== "Región de Tarapacá"){
+			console.log(sucursales[0].nombre)
+			$("#sucursales-printed").append(`
+			<div class="cuadro-sucursal col-md-5 col-lg-5 col-xs-12 id=`+ sucursales[0].id +`">
+                  <h4>`+ sucursales[0].nombre +`</h4>
+                  <p>`+ sucursales[0].title +`</p>
+                  <p>`+ sucursales[0].direccion +`</p>
+                  <p>`+ sucursales[0].comuna +`, `+ sucursales[0].region +`</p>
+                  <p><b>Horario:</b> `+ sucursales[0].horario +`</p>
+            </div>
+				`)
+		}
+		else if(($("#option_region").val())=== "Región de Atacama"){
+			console.log(sucursales[3].nombre)
+			$("#sucursales-printed").append(`
+			<div class="cuadro-sucursal col-md-5 col-lg-5 col-xs-12 id=`+ sucursales[3].id +`">
+                  <h4>`+ sucursales[3].nombre +`</h4>
+                  <p>`+ sucursales[3].title +`</p>
+                  <p>`+ sucursales[3].direccion +`</p>
+                  <p>`+ sucursales[3].comuna +`, `+ sucursales[3].region +`</p>
+                  <p><b>Horario:</b> `+ sucursales[3].horario +`</p>
+            </div>
+				`)
+		}
+		else if(($("#option_region").val())=== "Región de Magallanes"){
+			console.log(sucursales[55].nombre)
+			$("#sucursales-printed").append(`
+			<div class="cuadro-sucursal col-md-5 col-lg-5 col-xs-12 id=`+ sucursales[55].id +`">
+                  <h4>`+ sucursales[55].nombre +`</h4>
+                  <p>`+ sucursales[55].title +`</p>
+                  <p>`+ sucursales[55].direccion +`</p>
+                  <p>`+ sucursales[55].comuna +`, `+ sucursales[55].region +`</p>
+                  <p><b>Horario:</b> `+ sucursales[55].horario +`</p>
+            </div>
+				`)
+		}	
+	});
+});
